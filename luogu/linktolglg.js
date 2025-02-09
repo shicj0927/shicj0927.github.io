8871c66f2dbb5e4471e783b33ee7f0492768b4d5
shicj <1755887190@qq.com>
2025-02-09 09:49:20 +0800

    Update linktolglg.js

--- luogu/linktolglg.js
+++ luogu/linktolglg.js
@@ -1,34 +1,42 @@
 // ==UserScript==
 // @name         Link to lglg.top
 // @namespace    http://tampermonkey.net/
-// @version      0.2
-// @description  用lglg.top打开洛谷灌水区链接
+// @version      0.3
+// @description  ç”¨lglg.topæ‰“å¼€æ´›è°·çŒæ°´åŒºé“¾æŽ¥
 // @author       shicj
 // @match        https://www.luogu.com.cn/discuss?forum=problem
 // @match        https://www.luogu.com.cn/discuss?forum=academics
 // @match        https://www.luogu.com.cn/discuss?forum=relevantaffairs
 // @match        https://www.luogu.com.cn/discuss?forum=service
-// @match        https://www.luogu.com.cn/discuss?forum=P*
+// @match        https://www.luogu.com.cn/discuss?forum=*
+// @match        https://www.luogu.com.cn/problem/*
 // @grant        none
 // ==/UserScript==
 
-window.addEventListener('load',function() {
+// Copyright (c) 2025 shicj
+
+(function() {
+    /*
     const elements = document.querySelectorAll('*');
     elements.forEach(element => {
         const clone = element.cloneNode(true);
         element.parentNode.replaceChild(clone, element);
     });
-    const links = document.getElementsByTagName('a');
-    console.log("Link to lglg.top")
-    console.log(links)
-    for (let link of links) {
-        var h=link.href
-        if(h.includes("https://www.luogu.com.cn/discuss/")){
-            console.log(h)
-            link.href=h.replace("https://www.luogu.com.cn/discuss/","https://lglg.top/")
-            link.setAttribute('onclick','');
-            console.log(link.href)
+    */
+    setInterval(function(){
+        const links = document.getElementsByTagName('a');
+        console.log("Link to lglg.top")
+        console.log(links)
+        for (let link of links) {
+            var h=link.href
+            if(h.includes("https://www.luogu.com.cn/discuss/")){
+                console.log(h)
+                link.href=h.replace("https://www.luogu.com.cn/discuss/","https://lglg.top/")
+                link.setAttribute('onclick','');
+                console.log(link.href)
+                const clone = link.cloneNode(true);
+                link.parentNode.replaceChild(clone, link);
+            }
         }
-    }
-},false);
-
+    },1000);
+})();
